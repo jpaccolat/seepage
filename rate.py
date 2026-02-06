3eb95904af16d13d506cb84931ecd8247bf2fa85
Jonas Paccolat <jonas.paccolat@outlook.com>
2026-02-06 15:43:48 +0100

    Add positive stage formulas

--- rate.py
+++ rate.py
@@ -78,15 +78,6 @@ def q0_exact(cl_cond: float, cl_th: float, aq_cond: float,
         
     return q0
 
-def q_linear(stage: float, cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float, aq_para: str):
-    
-    q0 = q0_exact(cl_cond=cl_cond, cl_th=cl_th, aq_cond=aq_cond,
-            aq_scale=aq_scale, aq_shape=aq_shape, aq_para=aq_para)
-    q = q0 + stage / cl_th * cl_cond
-    
-    return q
-
 def q0_asymptote_negl(cl_cond: float,  cl_th: float, aq_cond: float,
              aq_scale: float, aq_shape: float, aq_para: str):
     
@@ -122,9 +113,10 @@ def q0_negl_to_soft(aq_cond, x, xi, a_ns):
 def q0_soft_to_hard(cl_cond, x, x_sh, xi, a_sh):
     return cl_cond * (1 + (x_sh / x)**a_sh)**(xi / a_sh)
 
-def q0_approximate_vGM(cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float, C_NS_1=-0.3850, C_NS_2=0.2056,
-             C_NS_3=0.5818, C_SH_1=0.4633, C_SH_2=0.5396):
+def q0_approximate_vGM(cl_cond, cl_th, aq_cond, aq_scale, aq_shape, 
+                       C_NS_1=-0.3850, C_NS_2=0.2056, C_NS_3=0.5818,
+                       C_SH_1=0.4633, C_SH_2=0.5396, C_NSH_1=1.05, C_NSH_2=0.25,
+                       C_NSH_3=6):
 
     b = 0.5 * (5 * aq_shape - 1)
     B = (1 - 1 / aq_shape)**2
@@ -139,15 +131,14 @@ def q0_approximate_vGM(cl_cond: float, cl_th: float, aq_cond: float,
     a_sh = C_SH_1 + C_SH_2 * xi
     q0_sh = q0_soft_to_hard(cl_cond, x, x_sh, xi, a_sh)
 
-    a_nsh = 1.05 + 0.25 * np.tanh(b - 6)
-    #a_nsh = 1.
+    a_nsh = C_NSH_1 + C_NSH_2 * np.tanh(b - C_NSH_3)
     s = 1 / (1 + (x_sh**0.5 / x)**a_nsh)
     q0 = q0_ns**(1-s) * q0_sh**s
 
     return q0
 
-def q0_approximate_BCB(cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float, C_SH_1=0.4633, C_SH_2=0.5396):
+def q0_approximate_BCB(cl_cond, cl_th, aq_cond, aq_scale, aq_shape,
+                       C_SH_1=0.4633, C_SH_2=0.5396):
 
     b = 2 + 3 * aq_shape
     B = 1
@@ -164,7 +155,7 @@ def q0_approximate_BCB(cl_cond: float, cl_th: float, aq_cond: float,
     return q0
 
 def q0_approximate(cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float, aq_para: str):
+                   aq_scale: float, aq_shape: float, aq_para: str):
     
     if aq_para == 'vGM':
         q0 = q0_approximate_vGM(cl_cond, cl_th, aq_cond, aq_scale, aq_shape)
@@ -173,8 +164,33 @@ def q0_approximate(cl_cond: float, cl_th: float, aq_cond: float,
 
     return q0
 
+def q_approximate_vGM(stage, cl_cond, cl_th, aq_cond, aq_scale, aq_shape):
+    
+    q0 = q0_approximate_vGM(cl_cond, cl_th, aq_cond, aq_scale, aq_shape)
+    
+    b = 0.5 * (5 * aq_shape - 1)
+    s = -cl_cond / cl_th * (q0 - cl_cond) / ((1+b) * q0 - cl_cond)
+    hc = cl_th * (aq_cond / cl_cond - 1)
+    hstar =  (q0 - cl_cond) / (s * hc + q0 - cl_cond) * hc
+    q = q0 + (cl_cond / cl_th - s * hstar / (stage - hstar)) * stage
+
+    return q
+
+def q_approximate_BCB(stage, cl_cond, cl_th, aq_cond, aq_scale, aq_shape):
+
+    q0 = q0_approximate_BCB(cl_cond, cl_th, aq_cond, aq_scale, aq_shape)
+
+    b = 2 + 3 * aq_shape
+    s = -cl_cond / cl_th * (q0 - cl_cond) / ((1+b) * q0 - cl_cond)
+    hc = cl_th * (aq_cond / cl_cond - 1) - aq_scale
+    cl_cond_cor = cl_cond * (1 + aq_scale / cl_th) 
+    hstar =  (q0 - cl_cond_cor) / (s * hc + q0 - cl_cond_cor) * hc
+    q = q0 + (cl_cond / cl_th - s * hstar / (stage - hstar)) * stage
+
+    return q
+
 def q_approximate(stage: float, cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float, aq_para: str):
+                  aq_scale: float, aq_shape: float, aq_para: str):
     
     if aq_para == 'vGM':
         q = q_approximate_vGM(stage, cl_cond, cl_th, aq_cond, aq_scale,
@@ -185,42 +201,7 @@ def q_approximate(stage: float, cl_cond: float, cl_th: float, aq_cond: float,
 
     return q
 
-def q_approximate_vGM(stage: float, cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float):
-    
-    q0 = q0_approximate_vGM(cl_cond, cl_th, aq_cond, aq_scale, aq_shape)
-    
-    b = 0.5 * (5 * aq_shape - 1)
-    s = q0 / cl_th * b / ((1 + b) * q0 / cl_cond - 1)
-    q = max(q0 + s * stage, cl_cond + cl_cond / cl_th * stage)
-
-    # hc = cl_th * (aq_cond / cl_cond - 1)
-    # A2 = cl_cond
-    # B2 = cl_cond / cl_th
-    # A1 = q0 - A2
-    # B1 = -A1 / hc
-    # hstar = A1 / (B1 + B2 - s)
-    # q = (A1 + B1 * stage) * np.exp(-stage/hstar) + (A2 + B2 * stage)
-
-    return q
-
-def q_approximate_BCB(stage: float, cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float):
-
-    q0 = q0_approximate_BCB(cl_cond, cl_th, aq_cond, aq_scale, aq_shape)
-
-    b = 2 + 3 * aq_shape
-    s = q0 / cl_th * b / ((1 + b) * q0 / cl_cond - 1)
-    B = cl_cond / cl_th
-    A = s - B
-    hc = cl_th * (aq_cond / cl_cond - 1) - aq_scale
-    hstar = -hc / np.log(((aq_cond - q0) / hc - B) / A) 
-    q = q0 + (A * np.exp(-stage / hstar) + B) * stage
-    
-    return q
-
-def q_modflow(stage: float, cl_cond: float, cl_th: float, aq_cond: float,
-             aq_scale: float, aq_shape: float, aq_para: str):
+def q_modflow(stage: float, cl_cond: float, cl_th: float):
 
     q = cl_cond * (1 + stage / cl_th)
 
